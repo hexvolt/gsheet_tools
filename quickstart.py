a6dcd5efb7184d06ee2cfa8dda4cdf8abb5c79a7
Eugene <ievgen.iermolenko@d1g1t.com>
2019-10-05 14:05:34 -0400

    initial setup and auth

---
+++ quickstart.py
@@ -0,0 +1,34 @@
+from googleapiclient.discovery import build
+
+from auth import get_credentials
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    credentials = get_credentials()
+
+    service = build('sheets', 'v4', credentials=credentials)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+
+if __name__ == '__main__':
+    main()
